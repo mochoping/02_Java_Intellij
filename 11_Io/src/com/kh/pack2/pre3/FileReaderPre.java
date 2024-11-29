5113ff03fec8528e665930fc6be512e69fd96e98
mochoping <hodolskm@gmail.com>
2024-11-29 17:18:16 +0900

    File FileWriter FileReader BufferedReader

---
+++ 11_Io/src/com/kh/pack2/pre3/FileReaderPre.java
@@ -0,0 +1,14 @@
+package com.kh.pack2.pre3;
+
+import java.io.FileReader;
+
+public class FileReaderPre {
+    /**
+     * readTxt(String Path, String fileName)
+     * @param path
+     * @param fileName
+     */
+    public void readTxt(){
+        FileReader fr = new FileReader(path,fileName)
+    }
+}
